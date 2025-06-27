e11c7cd81975a6feadd8ad590b54857860a03d6c
pratik yesankar <pratikiitkgp585@gmail.com>
2025-06-27 10:55:20 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,3 +1,1 @@
-export const leadSources = ["Website", "Referral", "Cold Call"]
-export const leadStatuses = ["New", "Contacted", "Qualified", "Lost"]
-export const priorities = ["Low", "Medium", "High"]
+ 
